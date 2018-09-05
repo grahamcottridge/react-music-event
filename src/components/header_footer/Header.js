4d4999e17907b338a481343bbcbed8d7e72e3779
Graham Cottridge <codinggc@gmail.com>
2018-09-05 22:50:41 +0100

    Add sidedrawer component to navbar

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -5,7 +5,20 @@ import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer';
+
 class Header extends Component {
+
+  state = {
+    drawerOpen: false
+  }
+
+  toggleDrawer = (value) => {
+    this.setState({
+      drawerOpen: value
+    })
+  }
+
   render() {
     return (
       <AppBar
@@ -22,8 +35,20 @@ class Header extends Component {
           <div className="header_logo">
             <div className="font_righteous header_logo_venue">The Venue</div>
             <div className="header_logo_title">Musical Events</div>
-
           </div>
+
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            onClick={() => this.toggleDrawer(true)}
+          >
+            <MenuIcon/>
+          </IconButton>
+
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={(value) => this.toggleDrawer(value)}
+          />
 
         </Toolbar>
       </AppBar>
