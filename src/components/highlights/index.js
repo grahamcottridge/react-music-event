17bf96adbebe4571a5690e415f7cd5ca77a0658b
Graham Cottridge <codinggc@gmail.com>
2018-09-08 22:16:20 +0100

    Create highligts section

---
+++ src/components/highlights/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Description from './Description';
+import Discount from './Discount';
+
+const Highlights = () => {
+    return (
+
+      <div className="highlight-wrapper">
+        <Description/>
+        <Discount/>
+      </div>
+
+    );
+};
+
+export default Highlights;
