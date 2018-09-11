ae127a8948c287ea587528fda629a7b5026b2182
Graham Cottridge <codinggc@gmail.com>
2018-09-11 18:37:27 +0100

    Add footer component

---
+++ src/components/header_footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Footer = () => {
+    return (
+
+        <footer className="bck-red">
+          <Fade delay={500}>
+            <div className="font-righteous footer-logo-venue">The Venue</div>
+            <div className="footer-copyright">The venue 2018. All rights reserved</div>
+          </Fade>
+        </footer>
+
+    );
+};
+
+export default Footer;
