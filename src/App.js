12dbd8d5635ec5bd295a5136ea3a68cdd8258ba0
Graham Cottridge <codinggc@gmail.com>
2019-11-30 23:02:43 +0000

    Fix minor issue with background color

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import './resources/styles.css';
-import { Element } from 'react-scroll';
+import React, { Component } from "react";
+import "./resources/styles.css";
+import { Element } from "react-scroll";
 
-import Header from './components/header_footer/Header';
-import Featured from './components/featured';
-import VenueNfo from './components/venueNfo';
-import Highlights from './components/highlights';
-import Pricing from './components/pricing';
-import Location from './components/location';
-import Footer from './components/header_footer/Footer';
+import Header from "./components/header_footer/Header";
+import Featured from "./components/featured";
+import VenueNfo from "./components/venueNfo";
+import Highlights from "./components/highlights";
+import Pricing from "./components/pricing";
+import Location from "./components/location";
+import Footer from "./components/header_footer/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{height: "1500px", background: "cornflowerblue"}}>
+      <div className="App" style={{ height: "1500px" }}>
         <Header />
 
         <Element name="featured">
@@ -43,4 +43,3 @@ class App extends Component {
 }
 
 export default App;
-
